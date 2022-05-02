3f152b3af5e8e6122e0b1c1dde7e1bb3ca161f98
semyeongJ <94214574+semyeongJ@users.noreply.github.com>
2022-05-03 00:56:14 +0900

    coin chalange

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos(currentArray => [toDo, ...currentArray]);
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [usd , setUsd] = useState(0);
+  const [getCoin, setGetCoin] = useState(0);
+  const onChange = (event) => {
+    setUsd(event.target.value);
+  }
+  const selectedCoin = (e) => {
+    setGetCoin(e.target.value)
+  }
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {  
+      setCoins(json)
+      setLoading(false);
+    });
+  },[]);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To do</button>
-      </form>
+      <h1>The coins! {loading? "": `(${coins.length})`}</h1>
+      <input onChange={onChange} type="number" placeholder="Write your USD.."/> $
       <hr/>
-      <ul>
-      {toDos.map((item, index) => (<li key={index}>{item}</li>))}
-      </ul>
+      {loading ? <strong>Loading...</strong> :
+      <select onChange={selectedCoin}>
+        <option>Select Coin</option>
+        {coins.map((coin,index) => (
+          <option key={index} value={coin.quotes.USD.price}> 
+            {coin.name} ({coin.symbol}): ${(coin.quotes.USD.price).toFixed(2)} USD
+          </option>))}
+      </select>
+        }
+        {getCoin === 0 ? null : 
+          <h3>구매 가능한 Coin 갯수: {(usd / getCoin).toFixed(2)} 개</h3>
+        }
     </div>
   );
 }
