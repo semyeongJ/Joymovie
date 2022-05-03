72931a9f2f78d1f383bcfd1a945b8b6ab1ea520b
semyeongJ <94214574+semyeongJ@users.noreply.github.com>
2022-05-04 02:42:10 +0900

    web start

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes ,Route } from "react-router-dom";
+import Detail from "./routes/Detail"
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [usd , setUsd] = useState(0);
-  const [getCoin, setGetCoin] = useState(0);
-  const onChange = (event) => {
-    setUsd(event.target.value);
-  }
-  const selectedCoin = (e) => {
-    setGetCoin(e.target.value)
-  }
-  useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {  
-      setCoins(json)
-      setLoading(false);
-    });
-  },[]);
   return (
-    <div>
-      <h1>The coins! {loading? "": `(${coins.length})`}</h1>
-      <input onChange={onChange} type="number" placeholder="Write your USD.."/> $
-      <hr/>
-      {loading ? <strong>Loading...</strong> :
-      <select onChange={selectedCoin}>
-        <option>Select Coin</option>
-        {coins.map((coin,index) => (
-          <option key={index} value={coin.quotes.USD.price}> 
-            {coin.name} ({coin.symbol}): ${(coin.quotes.USD.price).toFixed(2)} USD
-          </option>))}
-      </select>
-        }
-        {getCoin === 0 ? null : 
-          <h3>구매 가능한 Coin 갯수: {(usd / getCoin).toFixed(2)} 개</h3>
-        }
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/movie" element={<Detail/>}/>
+      </Routes>
+    </Router>
   );
 }
 
