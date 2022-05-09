f7979d655d30656703f824d1be35ef5355b826a4
semyeongJ <94214574+semyeongJ@users.noreply.github.com>
2022-05-10 01:21:57 +0900

    detail css

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import "../css/Detail.css";
+import Home from "./Home";
 
 function Detail() {
   const [loading, setLoading] = useState(true);
@@ -12,27 +13,35 @@ function Detail() {
     setMovie(json.data.movie);
     setLoading(false);
   };
+  const navigate = useNavigate();
+  const navigateBtn = () => {
+    navigate(-1);
+  };
   useEffect(() => {
     getMovie();
   }, []);
+
   return (
     <div className="container">
       {loading ? (
         <div className="loader">
-          <span className="loader_text">Loading...</span>
+          <span className="loader_text">Enjoy the movie!</span>
         </div>
       ) : (
-        <div style={{ height: "500px", width: "1000px" }}>
-          <h2>{movie.title_long}</h2>
-          <div style={{ float: "left", display: "inline" }}>
-            <img src={movie.medium_cover_image} alt={movie.title} />
+        <div className="detail">
+          <button onClick={navigateBtn} className="custom-btn btn-16">
+            back
+          </button>
+          <h2 className="detail_title">{movie.title_long}</h2>
+          <div className="detail_img">
+            <img src={movie.large_cover_image} alt={movie.title} title={movie.title} />
           </div>
-          <div style={{ wordBreak: "break-all", position: "relative", left: "30px" }}>
+          <div className="detail_data">
             <ul>
-              <li>Genre: {movie.genres.map((genre) => `${genre},`)}</li>
+              <li>Genre: {movie.genres.map((genre) => `${genre} `)}</li>
               <li>Language: {movie.language}</li>
               <li>Runtime: {movie.runtime} minutes</li>
-              <li>Rating: {movie.rating}/10</li>
+              <li>Rating: {movie.rating} / 10</li>
               <li>Summary: {movie.description_full}</li>
             </ul>
           </div>
